472e7183221c82cca30fb58023470c473ffbc48f
Mumshad Mannambeth <2704615+mmumshad@users.noreply.github.com>
2020-11-10 02:29:39 +0800

    Update workaround.sh

--- workaround.sh
+++ workaround.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #Apply on ControlPlane
+echo "Adding registry mirror on ControlPlane"
 systemctl stop docker
 sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json
 systemctl start docker
+
 #Apply on node01
+echo "Adding registry mirror on Node01"
 ssh -o strictHostKeyChecking=no node01 << EOF
 systemctl stop docker ; 
 sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json ; 
