94a6a549b522dd16f2550acc0ed269e6b8bf80b2
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2020-11-09 14:05:27 -0500

    Update workaround.sh

--- workaround.sh
+++ workaround.sh
@@ -3,7 +3,7 @@
 for j in `kubectl get nodes --no-headers| awk '{print $1}' | grep ^node`
 do
 echo "Adding registry mirror on $j"
-ssh -o strictHostKeyChecking=no $j << EOF
+ssh -q -o strictHostKeyChecking=no $j << EOF
 systemctl stop docker ; 
 sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json ; 
 systemctl start docker
@@ -17,5 +17,5 @@ sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/d
 systemctl start docker
 
 echo "Wait for Docker Service to be Ready"
-until kubectl get nodes 2>/dev/null | grep -w Ready 2> /dev/null; do  echo -n  .;sleep 1s; done
+until kubectl get nodes 2>/dev/null | grep -wq Ready 2> /dev/null; do  echo -n  .;sleep 1s; done
 
