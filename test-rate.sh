a500a1d3afab934c07ca19ba9a9d3752c314fb3c
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2020-11-09 15:02:48 -0500

    Create test-rate.sh

---
+++ test-rate.sh
@@ -0,0 +1,2 @@
+TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
+curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest
