b5679543cb497fcebdd2b0c9474a6bbb68f62d3d
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2020-11-25 23:43:20 -0500

    Update workaround.sh

--- workaround.sh
+++ workaround.sh
@@ -17,9 +17,9 @@ then
 echo "Adding registry mirror on ControlPlane"
 systemctl stop docker
 sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json
-systemctl start docker
+#systemctl start docker
 
 echo "Wait for Docker Service to be Ready"
-until kubectl get nodes 2>/dev/null | grep -wq Ready 2> /dev/null; do  echo -n  .;sleep 1s; done
+until kubectl get pods -n kube-system 2>/dev/null | grep  '0/' | wc -l | grep -qw 0 2> /dev/null; do  echo-n  .;sleep 1s; done
 fi
 
