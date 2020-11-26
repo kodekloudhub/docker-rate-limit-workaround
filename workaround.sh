f95149fb0ff5d34d89cedbb3cf9eeabd669f535e
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2020-11-25 23:56:23 -0500

    Update workaround.sh

--- workaround.sh
+++ workaround.sh
@@ -15,7 +15,7 @@ done
 if [ /etc/docker/daemon.json ]
 then
 echo "Adding registry mirror on ControlPlane"
-systemctl stop docker
+#systemctl stop docker
 sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json
 #systemctl start docker
 
