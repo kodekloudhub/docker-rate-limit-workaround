3cbaab2f1fe3548107210f4550b2fde0fc8a866e
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2020-12-02 12:13:07 -0500

    Update workaround.sh

--- workaround.sh
+++ workaround.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo
+echo " "
 #Apply on nodes
-for j in `kubectl get nodes --no-headers| awk '{print $1}' | grep ^node`
-do
-echo "Adding registry mirror on $j"
-ssh -q -o strictHostKeyChecking=no $j << EOF
-systemctl stop docker ; 
-sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json ; 
-systemctl start docker
-EOF
-done
+#for j in `kubectl get nodes --no-headers| awk '{print $1}' | grep ^node`
+#do
+#echo "Adding registry mirror on $j"
+#ssh -q -o strictHostKeyChecking=no $j << EOF
+#systemctl stop docker ; 
+#sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json ; 
+#systemctl start docker
+#EOF
+#done
 
 #Apply on ControlPlane
 #if [ /etc/docker/daemon.json ]
@@ -20,6 +20,6 @@ done
 #systemctl start docker
 
 #echo "Wait for Docker Service to be Ready"
-until kubectl get pods -n kube-system 2>/dev/null | grep  '0/' | wc -l | grep -qw 0 2> /dev/null; do  echo -n  .;sleep 1s; done
+#until kubectl get pods -n kube-system 2>/dev/null | grep  '0/' | wc -l | grep -qw 0 2> /dev/null; do  echo -n  .;sleep 1s; done
 #fi
 
