4f4e83157018341bc5bf15ace882a5df14966956
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2020-12-11 17:40:26 -0500

    Update workaround.sh

--- workaround.sh
+++ workaround.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo " "
 #Apply on nodes
-for j in `kubectl get nodes --no-headers| awk '{print $1}' | grep ^node`
-do
-echo "Adding registry mirror on $j"
-ssh -q -o strictHostKeyChecking=no $j << EOF
-systemctl stop docker ; 
-sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json ; 
-systemctl start docker
-EOF
-done
+#for j in `kubectl get nodes --no-headers| awk '{print $1}' | grep ^node`
+#do
+#echo "Adding registry mirror on $j"
+#ssh -q -o strictHostKeyChecking=no $j << EOF
+#systemctl stop docker ; 
+#sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json ; 
+#systemctl start docker
+#EOF
+#done
 
 #Apply on ControlPlane
 #if [ /etc/docker/daemon.json ]
