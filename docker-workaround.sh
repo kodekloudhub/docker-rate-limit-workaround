4561c7758ada7d34b5c7ab324a7c0fef3dcdb24a
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2021-02-22 10:06:22 -0500

    Update docker-workaround.sh

--- docker-workaround.sh
+++ docker-workaround.sh
@@ -13,14 +13,14 @@ EOF
 done
 
 #Apply on ControlPlane
-if [ /etc/docker/daemon.json ]
-then
-echo "Adding registry mirror on ControlPlane"
+#if [ /etc/docker/daemon.json ]
+#then
+#echo "Adding registry mirror on ControlPlane"
 #systemctl stop docker
 #sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json
-sed -i 's@http://docker-registry-mirror.katacoda.com@https://mirror.gcr.io@g' /etc/docker/daemon.json
-systemctl start docker
-sleep 5
-echo "Wait for Docker Service to be Ready"
-until kubectl get pods -n kube-system 2>/dev/null | egrep 'master|controlplane|weave|flannel' | grep  '0/' | wc -l | grep -qw 0 2> /dev/null; do  echo -n  .;sleep 1s; done
-fi
+#sed -i 's@http://docker-registry-mirror.katacoda.com@https://mirror.gcr.io@g' /etc/docker/daemon.json
+#systemctl start docker
+#sleep 5
+#echo "Wait for Docker Service to be Ready"
+sleep 5;until kubectl get pods -n kube-system 2>/dev/null | egrep 'master|controlplane|weave|flannel' | grep  '0/' | wc -l | grep -qw 0 2> /dev/null; do  echo -n  .;sleep 1s; done
+#fi
