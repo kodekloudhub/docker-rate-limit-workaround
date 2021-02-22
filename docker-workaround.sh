e69335caef580b7d7e3deb92657048cdb0619f80
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2021-02-21 22:34:17 -0500

    Create docker-workaround.sh

---
+++ docker-workaround.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo " "
+#Apply on nodes
+for j in `kubectl get nodes --no-headers| awk '{print $1}' | grep ^node`
+do
+echo "Adding registry mirror on $j"
+ssh -q -o strictHostKeyChecking=no $j << EOF
+systemctl stop docker ; 
+sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json ; 
+systemctl start docker
+EOF
+done
+
+#Apply on ControlPlane
+if [ /etc/docker/daemon.json ]
+then
+echo "Adding registry mirror on ControlPlane"
+systemctl stop docker
+sed -i '2i \    "registry-mirrors\": [\"https://mirror.gcr.io\"],' /etc/docker/daemon.json
+systemctl start docker
+sleep 5
+echo "Wait for Docker Service to be Ready"
+until kubectl get pods -n kube-system 2>/dev/null | egrep 'master|controlplane|weave|flannel' | grep  '0/' | wc -l | grep -qw 0 2> /dev/null; do  echo -n  .;sleep 1s; done
+fi
